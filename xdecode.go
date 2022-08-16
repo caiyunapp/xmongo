077d2526f044a2edc6b1709c7027ac56522f73fc
ringsaturn <ringsaturn.me@gmail.com>
2022-08-16 11:04:14 +0800

    update

--- xdecode.go
+++ xdecode.go
@@ -3,19 +3,38 @@ package xmongo
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func FindOne[T any](ctx context.Context, coll *mongo.Collection, query *bson.M, opts ...*options.FindOneOptions) (T, error) {
+	t := *new(T)
+	res := coll.FindOne(ctx, query, opts...)
+	if err := res.Err(); err != nil {
+		return *new(T), err
+	}
+	if err := res.Decode(t); err != nil {
+		return t, err
+	}
+	return t, nil
+}
+
+func Find[T any](ctx context.Context, coll *mongo.Collection, query *bson.M, opts ...*options.FindOptions) ([]T, error) {
+	cursor, err := coll.Find(ctx, query, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return Decode[T](ctx, cursor)
+}
+
 // Decode with generic support.
-func Decode[T any](ctx context.Context, cursor *mongo.Cursor, stopWhenDecodeErr bool) ([]T, error) {
+func Decode[T any](ctx context.Context, cursor *mongo.Cursor) ([]T, error) {
 	res := make([]T, 0)
 	for cursor.Next(ctx) {
 		newT := new(T)
 		if err := cursor.Decode(newT); err != nil {
-			if stopWhenDecodeErr {
-				return nil, err
-			}
-			continue
+			return nil, err
 		}
 		res = append(res, *newT)
 	}
