9b4cb3476f9731ddbfa577a86c2e83004d8cac62
ringsaturn <ringsaturn.me@gmail.com>
2022-09-02 23:04:44 +0800

    add Get/InsertOne/InsertMany

--- repo.go
+++ repo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,10 +19,26 @@ func NewRepo[M any](collection *mongo.Collection) (*Repo[M], error) {
 	}, nil
 }
 
+func (r Repo[M]) Get(ctx context.Context, oid primitive.ObjectID) (M, error) {
+	return r.FindOne(ctx, &bson.M{"_id": oid})
+}
+
 func (r Repo[M]) Find(ctx context.Context, query *bson.M, opts ...*options.FindOptions) ([]M, error) {
 	return Find[M](ctx, r.collection, query, opts...)
 }
 
 func (r Repo[M]) FindOne(ctx context.Context, query *bson.M, opts ...*options.FindOneOptions) (M, error) {
 	return FindOne[M](ctx, r.collection, query, opts...)
+}
+
+func (r Repo[M]) InsertOne(ctx context.Context, doc M, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
+	return r.collection.InsertOne(ctx, doc, opts...)
+}
+
+func (r Repo[M]) InsertMany(ctx context.Context, docs []M, opts ...*options.InsertManyOptions) (*mongo.InsertManyResult, error) {
+	var inputs []interface{} = make([]interface{}, len(docs))
+	for i, d := range docs {
+		inputs[i] = d
+	}
+	return r.collection.InsertMany(ctx, inputs, opts...)
 }
